159d9a6ad7a95c90b531dd98da111bd2c4e9dab2
Gio762002 <guo6437@outlook.com>
2024-02-01 18:50:36 +0100

    update functions

--- JavaScript/Jarnac.js
+++ JavaScript/Jarnac.js
@@ -1,15 +1,17 @@
 const prompt = require('prompt-sync')();
+var words = require('an-array-of-french-words')
+/*https://github.com/words/an-array-of-english-words*/
 
 const letter_pool = {"A":14,"B":4,"C":7,"D":5,"E":19,"F":2,"G":4,"H":2,"I":11,"J":1,"K":1,"L":6,"M":5,"N":9,"O":8,"P":4,"Q":1,"R":10,"S":7,"T":9,"U":8,"V":2,"W":1,"X":1,"Y":1,"Z":2};
-let letter_left = letter_pool;
+let letters_left = letter_pool;
 // var init = false;
 
 /*
-get 6 random letters from the bag if it concerns the first hand of the game or 1 letter if not
-return: an array of 6 letters or a letter
+ * get 6 random letters from the bag if it concerns the first hand of the game or 1 letter if not
+ * return: an array of 6 letters or a letter
  */
 function get_random_letters(init) {
-  const letters = Object.keys(letter_left);
+  const letters = Object.keys(letters_left);
   const random_letters = [];
   let nb_letters = 1;
   if (init) { nb_letters = 6; } 
@@ -17,13 +19,13 @@ function get_random_letters(init) {
   for (let i = 0; i < nb_letters ; i++) {
     let letter = '';
     
-    while (letter === '' || letter_left[letter] === 0) {
+    while (letter === '' || letters_left[letter] === 0) {
       const random_index = Math.floor(Math.random() * letters.length);
       letter = letters[random_index];
     }
     
     random_letters.push(letter);
-    letter_left[letter]--;
+    letters_left[letter]--;
   }
   
   return random_letters;
@@ -44,8 +46,8 @@ let player1 = {
   words_played : []
 }
 let player2 = {
-  plate : [[[],[],[],[],[],[],[],[],[]],
-           [[],[],[],[],[],[],[],[],[]],
+  plate : [[['M'],['O'],['M'],[],[],[],[],[],[]],
+           [['D'],['A'],['D'],[],[],[],[],[],[]],
            [[],[],[],[],[],[],[],[],[]],
            [[],[],[],[],[],[],[],[],[]],
            [[],[],[],[],[],[],[],[],[]],
@@ -56,40 +58,101 @@ let player2 = {
   words_played : []
 }
 
+/*
+ * change the letters chosen with the bag, no return
+ */ 
+function exchange_with_bag(player,letters, letters_left) {
+  for (let letter of letters) {
+    player.hand.push(get_random_letters(false)[0]);
+    if (letters_left[letter] === undefined) {
+      letters_left[letter] = 0;
+    }
+    letters_left[letter]++;
+  }
+}
+
+/*
+ * choose a place to put the letter in the plate
+ * return: (type,row); type =1 or 2 correspond to the next action the player should do 
+ */
+function get_aimed_row(player) {
+  console.log('choose a row (0-7) to put the letter');
+  let x = parseInt(prompt('x: '));
+  if (x < 0 || x > 7) {
+    console.log('this row is outside the plate, choose again');
+    return get_locat(player);
+  } else {
+    if (player.plate[x][0].length === 0) {
+      let aime = x;
+      for (let i = x; i > 0 ; i--) {
+        if (player.plate[i][0].length === 0){ aime = i;}
+      }
+      console.log('you try to put a letter in an empty row, you have to fill the row from the top');
+      console.log('please choose at least 3 letters in your hand to put in this row');
+      return [1, aime];
+    } else {
+      console.log('you try to put a letter in a row with letters, please choose a letter in your hand to put in this row');
+      return [2, x];
+    }
+  }
+}
+
+
 /* 
-choose a letter from hand to put in the plate or three letters to exchange with the bag
-return: a letter or an array of three letters
-*/
-function letter_aimed(player,type){
-  if (type === 'put') {
-    let letter = prompt('choose a letter:');
+ * choose a letter from hand to put in the plate or three letters to exchange with the bag
+ * return: a letter or an array of three letters
+ */
+function get_aimed_letter(player,type){
+  if (type === '2') {
+    let letter = prompt('choose a letter: ');
     if (player.hand.includes(letter)) {
       const index = player.hand.indexOf(letter);
       player.hand.splice(index, 1);
       return letter;
     } else {
       console.log('you do not have this letter in your hand, try again');
-      return letter_aimed(player,'put');
+      return get_aimed_letter(player,'put');
     }
+  }
+  else if (type === '1') {
+    console.log('choose 3 letters to put');
+    return [get_aimed_letter(player,'put'),get_aimed_letter(player,'put'),get_aimed_letter(player,'put')];
   }
   else if (type === 'exchange') {
     console.log('choose 3 letters to exchange');
-    return [letter_aimed(player,'put'),letter_aimed(player,'put'),letter_aimed(player,'put')];
+    return [get_aimed_letter(player,'put'),get_aimed_letter(player,'put'),get_aimed_letter(player,'put')];
   }
 }
 
 /*
-change the letters chosen with the bag, no return
-*/ 
-function exchange_with_bag(player,letters, letter_left) {
-  for (let letter of letters) {
-    player.hand.push(get_random_letters(false)[0]);
-    if (letter_left[letter] === undefined) {
-      letter_left[letter] = 0;
+ * rearrange the letters in input
+ * return: an array of letters rearranged
+ */
+function rearrange_letters(letters) {
+  let arranged_letters = [];
+  console.log('enter all the letters in the row by your order: ', letters);
+  while (letters.length > 0) {
+    let letter = prompt('letter: ');
+    const index = letters.indexOf(letter);
+    if (index !== -1) {
+      letters.splice(index, 1);
+      arranged_letters.push(letter);
+    } else {
+      console.log('Invalid letter. Please try again.');
     }
-    letter_left[letter]++;
   }
+  return arranged_letters;
 }
+console.log(rearrange_letters(['A','B','C','D','E','F']));
+
+/*
+ * check if the word is valid, return true or false
+ */
+function verify_word(word) {
+}
+
+
+
 
 
 function put_letter(letter, player, x, y) {
@@ -97,9 +160,9 @@ function put_letter(letter, player, x, y) {
   y = parseInt(y);
   if (Array.isArray(player.plate[x][y]) && player.plate[x][y].length > 0) {
     console.log('this cell is already occupied,tried another one');
-    let newLetter = prompt('letter:');
-    let newX = parseInt(prompt('x:'));
-    let newY = parseInt(prompt('y:'));
+    let newLetter = prompt('letter: ');
+    let newX = parseInt(prompt('x: '));
+    let newY = parseInt(prompt('y: '));
     put_letter(newLetter, player, newX, newY);
   } else {
     player.plate[x][y] = [letter];
@@ -116,7 +179,3 @@ function display_plate(player) {
     console.log(rowStr);
   }
 }
-// put_letter('A', player2, 0, 0);
-// display_plate(player2);
-// put_letter('B', player2, 0, 0);
-// display_plate(player2);
