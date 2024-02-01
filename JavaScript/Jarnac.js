e8651f27949e1defe1d3ad6aaf237038fc19bd76
Gio762002 <guo6437@outlook.com>
2024-02-01 20:45:04 +0100

    update

--- JavaScript/Jarnac.js
+++ JavaScript/Jarnac.js
@@ -2,7 +2,7 @@ const prompt = require('prompt-sync')();
 var words = require('an-array-of-french-words').map(word => word.toUpperCase());
 /*https://github.com/words/an-array-of-english-words*/
 
-const letter_pool = {"A":14,"B":4,"C":7,"D":5,"E":19,"F":2,"G":4,"H":2,"I":11,"J":1,"K":1,"L":6,"M":5,"N":9,"O":8,"P":4,"Q":1,"R":10,"S":7,"T":9,"U":8,"V":2,"W":1,"X":1,"Y":1,"Z":2};
+const letter_pool = { "A": 14, "B": 4, "C": 7, "D": 5, "E": 19, "F": 2, "G": 4, "H": 2, "I": 11, "J": 1, "K": 1, "L": 6, "M": 5, "N": 9, "O": 8, "P": 4, "Q": 1, "R": 10, "S": 7, "T": 9, "U": 8, "V": 2, "W": 1, "X": 1, "Y": 1, "Z": 2 };
 let letters_left = letter_pool;
 // var init = false;
 
@@ -14,56 +14,28 @@ function get_random_letters(init) {
   const letters = Object.keys(letters_left);
   const random_letters = [];
   let nb_letters = 1;
-  if (init) { nb_letters = 6; } 
+  if (init) { nb_letters = 6; }
 
-  for (let i = 0; i < nb_letters ; i++) {
+  for (let i = 0; i < nb_letters; i++) {
     let letter = '';
-    
+
     while (letter === '' || letters_left[letter] === 0) {
       const random_index = Math.floor(Math.random() * letters.length);
       letter = letters[random_index];
     }
-    
+
     random_letters.push(letter);
     letters_left[letter]--;
   }
-  
+
   return random_letters;
 }
 
 
-// need to seperate to a main js file
-let player1 = {
-  name : 'player1',
-  plate : [[[],[],[],[],[],[],[],[],[]],
-           [[],[],[],[],[],[],[],[],[]],
-           [[],[],[],[],[],[],[],[],[]],
-           [[],[],[],[],[],[],[],[],[]],
-           [[],[],[],[],[],[],[],[],[]],
-           [[],[],[],[],[],[],[],[],[]],
-           [[],[],[],[],[],[],[],[],[]],
-           [[],[],[],[],[],[],[],[],[]]],
-  hand : ['A','B','C','D','E','F'],
-  words_played : []
-}
-let player2 = {
-  name : 'player2',
-  plate : [[['M'],['O'],['M'],[],[],[],[],[],[]],
-           [['D'],['A'],['D'],[],[],[],[],[],[]],
-           [[],[],[],[],[],[],[],[],[]],
-           [[],[],[],[],[],[],[],[],[]],
-           [[],[],[],[],[],[],[],[],[]],
-           [[],[],[],[],[],[],[],[],[]],
-           [[],[],[],[],[],[],[],[],[]],
-           [[],[],[],[],[],[],[],[],[]]],
-  hand : ['F','R','E','N','C','H'],
-  words_played : []
-}
-
 /*
  * change the letters chosen with the bag, no return
- */ 
-function exchange_with_bag(player,letters, letters_left) {
+ */
+function exchange_with_bag(player, letters, letters_left) {
   for (let letter of letters) {
     player.hand.push(get_random_letters(false)[0]);
     if (letters_left[letter] === undefined) {
@@ -86,8 +58,8 @@ function get_aimed_row(player) {
   } else {
     if (player.plate[x][0].length === 0) {
       let aime = x;
-      for (let i = x; i > 0 ; i--) {
-        if (player.plate[i][0].length === 0){ aime = i;}
+      for (let i = x; i > 0; i--) {
+        if (player.plate[i][0].length === 0) { aime = i; }
       }
       console.log('you try to put a letter in an empty row, you have to fill the row from the top');
       console.log('please choose at least 3 letters in your hand to put in this row');
@@ -104,7 +76,7 @@ function get_aimed_row(player) {
  * choose a letter from hand to put in the plate or three letters to exchange with the bag
  * return: a letter or an array of three letters
  */
-function get_aimed_letter(player,type){
+function get_aimed_letter(player, type) {
   if (type === '2') {
     let letter = prompt('choose a letter: ');
     if (player.hand.includes(letter)) {
@@ -113,16 +85,16 @@ function get_aimed_letter(player,type){
       return letter;
     } else {
       console.log('you do not have this letter in your hand, try again');
-      return get_aimed_letter(player,'put');
+      return get_aimed_letter(player, 'put');
     }
   }
   else if (type === '1') {
     console.log('choose 3 letters to put');
-    return [get_aimed_letter(player,'put'),get_aimed_letter(player,'put'),get_aimed_letter(player,'put')];
+    return [get_aimed_letter(player, 'put'), get_aimed_letter(player, 'put'), get_aimed_letter(player, 'put')];
   }
   else if (type === 'exchange') {
     console.log('choose 3 letters to exchange');
-    return [get_aimed_letter(player,'put'),get_aimed_letter(player,'put'),get_aimed_letter(player,'put')];
+    return [get_aimed_letter(player, 'put'), get_aimed_letter(player, 'put'), get_aimed_letter(player, 'put')];
   }
 }
 
@@ -184,7 +156,7 @@ function display_plate(player) {
   for (let row of player.plate) {
     let rowStr = '';
     for (let cell of row) {
-      rowStr += (Array.isArray(cell) && cell.length === 0) ? ' [ ] ' :  ' ['+cell+'] ';
+      rowStr += (Array.isArray(cell) && cell.length === 0) ? ' [ ] ' : ' [' + cell + '] ';
     }
     console.log(rowStr);
   }
@@ -197,4 +169,15 @@ function display_plate(player) {
 function display_hand(player) {
   console.log('hand of player ', player.name, ': ', player.hand);
 }
-display_hand(player1);
+
+module.exports = {
+  get_random_letters,
+  exchange_with_bag,
+  get_aimed_row,
+  get_aimed_letter,
+  rearrange_letters,
+  verify_word,
+  put_word,
+  display_plate,
+  display_hand
+};
